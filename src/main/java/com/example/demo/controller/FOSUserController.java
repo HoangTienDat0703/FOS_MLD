296ab185cc71c3a55d6b55e3bc23895232820d11
HoangTienDat0703 <tiendat2000zx@gmail.com>
2022-06-25 21:32:55 +0700

    2506l2

--- src/main/java/com/example/demo/controller/FOSUserController.java
+++ src/main/java/com/example/demo/controller/FOSUserController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@CrossOrigin
+@CrossOrigin(origins = "https://project-for-fos-mld.herokuapp.com")
 @RestController
 @RequestMapping("/api")
 public class FOSUserController {
